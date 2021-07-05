8aed2039e9dd61603e2a1bde3a0a4f5eb6d41031
James Webber <james.webber@cansfordlabs.co.uk>
2021-07-05 09:15:19 +0100

    updated with commands to change views

--- ModernDesign/MVVM/ViewModel/MainViewModel.cs
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,33 @@ namespace ModernDesign.MVVM.ViewModel
 {
     public class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
 
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM; });
+            DiscoveryViewCommand = new RelayCommand(o => { CurrentView = DiscoveryVM; });
         }
     }
 }
